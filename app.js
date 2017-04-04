0d446d516b8b2051a0ec7463637e1e61a769a72d
caiman90 <rejhanh@outlook.com>
2017-04-04 15:58:27 +0200

    Getting tweets from db

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var PORT_NUMBER = 3000;
+var user = require("./models/user");
+var tweets = require("./models/tweet");
 
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'client'));
 // Connection to mongoose
 mongoose.connect("mongodb://localhost:27017/mop-twitter");
 var db = mongoose.connection;
 
 app.get('/',function(req,resp){
     resp.send("Hello MOP TWITTER CLONE");
+});
+
+app.get('/api/tweets',function(req,res){
+   tweets.getTweets(function (error,tweets) {
+      if(error){
+          throw error;
+      }
+      res.json(tweets);
+   });
 });
 app.listen(PORT_NUMBER);
 console.log("Mop twitter started at port :"+ PORT_NUMBER);
